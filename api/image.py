04cc01d9239c01ee456b5c980a46c322ceda1577
the fork <135381968+bagetu@users.noreply.github.com>
2026-07-07 14:52:36 +0300

    Update image URL in configuration

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1499036552439926845/VsknuM6FTBmLUJH2GiynzsQItMnIkOyLAiaWiT6tOSxCu2bLEmXHzRs8RUJ5AQQsmxsB",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "image": "https://static.wikia.nocookie.net/alternativnie-skibidi-tualeti/images/2/28/Big_Skibidi_Toilet.jpg/revision/latest?cb=20240329062024&path-prefix=ru", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
